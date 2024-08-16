1479a033579be23749f7b93de562bf5907c46bca
Bence <m.bence211@gmail.com>
2024-08-16 15:20:56 +0200

    additional set up and initial styling

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/navigation";
-
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 export const metadata = {
 	title: "The Wild Oasis",
 	description: "Cabin renting webpage",
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>
+			<body className="bg-primary-950 min-h-screen text-primary-100">
 				<header>
 					<Logo />
 					<Navigation />
