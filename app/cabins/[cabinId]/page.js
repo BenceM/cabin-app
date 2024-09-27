751f89304f5f7c897ad8f9a3ed9ede0e71bdd244
Bence <m.bence211@gmail.com>
2024-09-27 13:50:32 +0200

    about page show more implemented

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -27,7 +28,7 @@ export default async function Page({ params }) {
 					<Image
 						src={image}
 						fill
-						className="object-cover "
+						className="object-cover"
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 						alt={`Cabin ${name}`}
 					/>
@@ -38,7 +39,9 @@ export default async function Page({ params }) {
 						Cabin {name}
 					</h3>
 
-					<p className="text-lg text-primary-300 mb-10">{description}</p>
+					<p className="text-lg text-primary-300 mb-10">
+						<TextExpander>{description}</TextExpander>
+					</p>
 
 					<ul className="flex flex-col gap-4 mb-7">
 						<li className="flex gap-3 items-center">
