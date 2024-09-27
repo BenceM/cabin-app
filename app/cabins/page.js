212dd84d89e3335e2debc8d4bab789b78f0cd234
Bence <m.bence211@gmail.com>
2024-09-27 15:29:40 +0200

    added a filter for cabins storing state in the params

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,17 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
 	title: "Cabins",
 };
+//after adding the searchParams there is no need to revalidate the page like this because the page will be dynamic
 export const revalidate = 3600;
 //re fetches every hour
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+	const filter = searchParams?.capacity ?? "all";
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +25,11 @@ export default async function Page() {
 				home away from home. The perfect spot for a peaceful, calm vacation.
 				Welcome to paradise.
 			</p>
-			<Suspense fallback={<Spinner />}>
-				<CabinList />
+			<div className="flex justify-end mb-8">
+				<Filter />
+			</div>
+			<Suspense fallback={<Spinner />} key={filter}>
+				<CabinList filter={filter} />
 			</Suspense>
 		</div>
 	);
