a243aeb2aadec5f98e673a66fd70809847e00b59
Bence <m.bence211@gmail.com>
2024-10-01 19:35:31 +0200

    custom api endpoint for get

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -10,6 +10,26 @@ function ReservationReminder() {
 
 	if (!range.from || !range.to) return null;
 	// NEEDS ON OUTSIDE CLICK IMPLEMENTED
+	// import { useEffect, useRef } from "react";
+
+	// export default function useOutsideClick(handler, listenCapturing = true) {
+	//   const ref = useRef();
+
+	//   useEffect(
+	//     function () {
+	//       function handleClick(e) {
+	//         if (ref.current && !ref.current.contains(e.target)) handler();
+	//       }
+	//       document.addEventListener("click", handleClick, listenCapturing);
+
+	//       return () =>
+	//         document.removeEventListener("click", handleClick, listenCapturing);
+	//     },
+	//     [handler, listenCapturing],
+	//   );
+	//   return ref;
+	// }
+
 	return (
 		<div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
 			<p>
