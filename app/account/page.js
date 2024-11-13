3f4b10c9df5d82c546566045271e6b37d29fe3b2
Bence <m.bence211@gmail.com>
2024-11-13 20:00:03 +0100

    user data fetching for form

--- app/account/page.js
+++ app/account/page.js
@@ -6,8 +6,6 @@ export const metadata = {
 };
 
 export default async function Page() {
-	const session = await auth();
-	console.log(session);
 	return (
 		// <Suspense fallback={<Spinner />}>
 		<LoginNameDisplay />
