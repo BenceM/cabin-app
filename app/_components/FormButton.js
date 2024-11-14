670ac9aece638afbddf081a2d1d85c8ded4cccdc
Bence <m.bence211@gmail.com>
2024-11-14 20:59:14 +0100

    fixed button size

--- app/_components/FormButton.js
+++ app/_components/FormButton.js
@@ -5,14 +5,19 @@ import SpinnerMini from "./SpinnerMini";
 
 export default function FormButton() {
 	const { pending } = useFormStatus();
-	console.log(pending);
 
 	return (
 		<button
-			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+			className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300 flex items-center justify-center"
 			disabled={pending}
 		>
-			{pending ? <SpinnerMini /> : "Update profile"}
+			{pending ? (
+				<div className="flex items-center justify-center px-8">
+					<SpinnerMini />
+				</div>
+			) : (
+				"Update profile"
+			)}
 		</button>
 	);
 }
